9add4f68122c9926d90f8f48d87e04a0a504bcd9
Aaron Goin <aaron.goin@wsu.edu>
2017-04-07 22:22:49 -0700

    working on the lightbox implementation. (currently called viewBox because I couldn't remember the exact term. ;P )

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 (function() {
 
 	var lastSearch = "",
-		waitingForResults = false;
+		haveAllResults = true,
+		waitingForResults = false,
+		scrolledAmount = 0;
 
 	const
 		searchField = document.getElementById('searchField'),
@@ -20,11 +22,11 @@
 							break;
 						case 204: // no additional results for search
 							feedback.innerHTML = "No more results.";
-							window.removeEventLister('change', onScroll);
+							haveAllResults = true;
 							break;
 						case 404: // no results at all for search string
-							feedback.innerHTML = "No results.";se
-							window.removeEventLister('change', onScroll);
+							feedback.innerHTML = "No results.";
+							haveAllResults = true;
 							break;
 					}
 				}
@@ -39,13 +41,14 @@
 		onSearch = function(event) {
 			if (searchField.value === "") {
 				lastSearch = "";
-				feedback.innerHTML = ""
+				feedback.innerHTML = "You hungry?";
+				resultsContainer.innerHTML = "";
 			} else if (searchField.value !== lastSearch) {
 				waitingForResults = true;
 				lastSearch = searchField.value;
 				feedback.innerHTML = "Waiting for results..."
+				haveAllResults = false;
 				Search(searchField.value, resultsContainer.children.length, 10);
-				window.addEventListener('scroll', onScroll);
 			}
 
 		},
@@ -54,18 +57,17 @@
 		onScroll = function(event) {
 		
 			var totalHeight,
-				visibleHeight,
-				scrolledAmount;
-
-			if (waitingForResults) return;
-
-			totalHeight = document.body.clientHeight;
-			visibleHeight = window.innerHeight;
+				visibleHeight;
 
 			// get amount user has scrolled down (in pixels)
 			if (window.pageYOffset !== undefined) scrolledAmount = window.pageYOffset;
 			else if (document.documentElement) scrolledAmount = document.documentElement.scrollTop;
 			else scrolledAmount = document.body.scrollTop;
+
+			if (waitingForResults || haveAllResults || searchField.value === "") return;
+
+			totalHeight = document.body.clientHeight;
+			visibleHeight = window.innerHeight;
 
 			// if user close enough to bottom: get more results
 			if ((totalHeight - scrolledAmount) < (visibleHeight + 200)) {
@@ -73,14 +75,28 @@
 				Search(lastSearch, resultsContainer.children.length, 10);
 			}
 		},
+// NUTRITIONAL FACTS 
+		getNutritionalInfo = function() {
+			// TODO
+		},
+// EXPANDED VIEWBOX
+		showViewBox = function(event) {
+			// event.target should point to img
+			if (event.target !== event.currentTarget) {
+				document.body.className = "noscroll";
+				viewBox.style.top = scrolledAmount + "px";
+				viewBox.firstElementChild.src = event.target.src;
+				viewBox.children[1].innerHTML = event.target.previousElementSibling.innerHTML;
+				viewBox.style.display = "block";
+			}
+			
+		},
 		hideViewBox = function(event) {
 			// make sure user clicked viewBox and not any of it's children
 			if (event.target === event.currentTarget) {
 				event.target.style.display = "none";
+				document.body.className = "";
 			}
-		},
-		showViewBox = function(event) {
-			// event.target should point to img
 		};
 	
 // HOOK EVERYTHING UP
@@ -88,6 +104,7 @@
 	searchField.addEventListener('change', onSearch);
 	viewBox.addEventListener('click', hideViewBox);
 	resultsContainer.addEventListener('click', showViewBox);
+	window.addEventListener('scroll', onScroll);
 
 
 	window.onbeforeunload = function() {
