778bed36727d78d0cfbf77df6439cbff2176a2fc
Aaron Goin <aaron.goin@wsu.edu>
2017-04-20 08:54:51 -0700

    Front-end mostly complete. Moved front-end files into Flask templates. Modified server to send different status codes for different results.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request
+from flask.ext.api import status
 from searcher import *
 
 app = Flask(__name__)
@@ -6,24 +7,43 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	print "Someone is at the home page."
-	return render_template('welcome_page.html')
+	return render_template('index.html')
 
-@app.route('/questions/')
+@app.route('/description/', methods=['POST'])
 def my_link():
-	print 'I got clicked!'
-	return 'Click.'
+	index = request.form.get('n')
+	print index
 
-@app.route('/results/', methods=['GET', 'POST'])
+	# split string description into list of paragraphs
+	body = PLEASE_REPLACE_ME[index][3].splitlines()
+	if not body:
+		return '', status.HTTP_404_NOT_FOUND
+	else:
+		return render_template('description.html', body=body), status.HTTP_200_OK
+
+@app.route('/results/', methods=['POST'])
 def results():
-	# if request.method == 'POST':
-	# 	data = request.form
-	# else:
-	# 	data = request.args
+	# params:
+	# query (search terms)
+	query = request.form.get('query')
+	# index (how many results the client already has)
+	index = request.form.get('index')
+	# count (how many results the client wants back)
+	count = request.form.get('count')
+
 	indexer = open_dir("indexDir")
-	searcher = request.form.get('searchterm')
-	results = search(indexer, searcher)
+	
+	results = search(indexer, query)
 	print(results)
-	return render_template('results.html', query=searcher, results=results)
+	if not results:
+		# no results for search
+		return '', status.HTTP_404_NOT_FOUND
+	else if len(results) is count:
+		# client already has all results
+		return '', status.HTTP_204_NO_CONTENT
+	else:
+		# render results for client
+		return render_template('results.html', results=results[index:count]), status.HTTP_200_OK
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ static/scripts/index.js
@@ -0,0 +1,216 @@
+(function() {
+
+	var MAX_RESULTS = 20,
+		MIN_RESULTS = 6,
+		NETWORK = {
+			avg: 250, // network latency in milliseconds
+			n: 1 // number of requests sent (starts at 1 so we don't divide by 0)
+		},
+		lastSearch = "",
+		haveAllResults = true,
+		waitingForResults = false,
+		scrolledAmount = 0;
+
+	const
+		searchField = document.getElementById('searchField'),
+		resultsContainer = document.getElementById('resultsContainer'),
+		feedback = document.getElementById('feedback'),
+		viewBox = document.getElementById('viewBox'),
+		viewBoxBtn = document.getElementById('viewBoxBtn'),
+		boxTitle = document.getElementById('boxTitle'),
+		boxImg = document.getElementById('boxImg'),
+		boxNutrition = document.getElementById('boxNutrition'),
+		boxDetails = document.getElementById('boxDetails'),
+
+		speedTest = function(delta) {
+			// get time passed 
+			delta = window.performance.now() - delta;
+			// calculate cumulative average
+			NETWORK.avg = (delta + NETWORK.avg * NETWORK.n) / ++(NETWORK.n);
+			// MAX_RESULTS is larger of:
+			// square root of average latency truncated into an integer
+			// or 6
+			MAX_RESULTS = Math.max( Math.sqrt(NETWORK.avg)|0, MIN_RESULTS);
+
+			//console.log("Network avg latency: " + NETWORK.avg + "\nResults max: " + MAX_RESULTS);
+		},
+
+		appendResults = function(results) {
+		// using documentFragments and appending avoids resetting the entire DOM and preserves scroll position
+		// but data retrieved from server as html string and needs to be parsed before appending
+			
+			var temp = document.createElement('div'),
+				fragment = document.createDocumentFragment();
+			
+			// parse results into DOM Nodes using temp div
+			temp.innerHTML = results;
+			
+			// move now parsed results from the div into the fragment
+			while (temp.firstElementChild) fragment.appendChild(temp.removeChild(temp.firstElementChild));
+
+			// appending a documentFragment into the DOM replaces itself with it's children
+			// leaving no trace of the fragment in the live DOM
+			resultsContainer.appendChild(fragment);
+		}
+
+		requestSearch = function(string, index, count) {
+
+			var request = new XMLHttpRequest(),
+				delta;
+
+			request.onreadystatechange = function () {
+				if (request.readyState === XMLHttpRequest.DONE) {
+					speedTest(delta);
+					switch (request.status) {
+						case 200: // request returned successfully
+							appendResults(request.response);
+							waitingForResults = false;
+							break;
+						case 204: // no additional results for search
+							feedback.innerHTML = "No more results.";
+							haveAllResults = true;
+							break;
+						case 404: // no results at all for search string
+							feedback.innerHTML = "No results.";
+							haveAllResults = true;
+							break;
+						default:
+							feedback.innerHTML = "Cannot complete search.";
+							break;
+					}
+				}
+		    };
+			request.open('POST', '/search', true);
+			request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			request.responseType = 'text';
+			request.send('query=' + string + '&index=' + index + '&count=' + count);
+
+			// time our request
+			// performance.now() is a DOMHighResTimeStamp, measured in milliseconds, accurate to five thousandths of a millisecond
+			delta = window.performance.now();
+		},
+
+		requestDescription = function(index) {
+			var request = new XMLHttpRequest(),
+				delta;
+
+			request.onreadystatechange = function () {
+				if (request.readyState === XMLHttpRequest.DONE) {
+					speedTest(delta);
+					switch (request.status) {
+						case 200: // request returned successfully
+							boxDetails.innerHTML = request.response;
+							break;
+						default:
+							boxDetails.innerHTML = 'Cannot retrieve description.';
+							break;
+					}
+				}
+		    };
+			request.open('POST', '/description', true);
+			request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			request.responseType = 'text';
+			request.send('n=' + index);
+
+			delta = window.performance.now();
+		}
+
+		handleSearch = function(event) {
+
+			// empty search resets page
+			if (searchField.value === "") {
+
+				lastSearch = "";
+				feedback.innerHTML = "You hungry?";
+				resultsContainer.innerHTML = "";
+
+			} else if (searchField.value !== lastSearch) {
+
+				waitingForResults = true;
+				lastSearch = searchField.value;
+				feedback.innerHTML = "Waiting for results..."
+				haveAllResults = false;
+
+				requestSearch(searchField.value, resultsContainer.children.length, MAX_RESULTS);
+			}
+
+		},
+
+		getMoreResults = function(event) {
+		
+			var totalHeight,
+				visibleHeight;
+
+			// get amount user has scrolled down (in pixels)
+			if (window.pageYOffset !== undefined) scrolledAmount = window.pageYOffset;
+			else if (document.documentElement) scrolledAmount = document.documentElement.scrollTop;
+			else scrolledAmount = document.body.scrollTop;
+
+			if (waitingForResults || haveAllResults || searchField.value === "") return;
+
+			totalHeight = document.body.clientHeight;
+			visibleHeight = window.innerHeight;
+
+			// if user close enough to bottom: get more results
+			if ((totalHeight - scrolledAmount) < (visibleHeight + 200)) {
+				waitingForResults = true;
+				requestSearch(lastSearch, resultsContainer.children.length, MAX_RESULTS);
+			}
+
+			return false;
+		},
+
+		getNutritionalInfo = function() {
+			// TODO
+			return "Such nutrition!</br>Much wow!";
+		},
+
+		showViewBox = function(event) {
+			// event.target should point to img
+			if (event.target !== event.currentTarget) {
+				document.body.className = "noscroll";
+				viewBox.style.top = scrolledAmount + "px";
+				boxImg.src = event.target.src; // set image source
+				boxTitle.innerHTML = event.target.previousElementSibling.innerHTML;
+				boxNutrition.innerHTML = getNutritionalInfo();
+				boxDetails.innerHTML = "Retrieving details...";
+
+				// request full text from server
+				requestDescription(event.target.parentElement.dataset.index);
+
+				// this keeps our image scaled the right way
+				if (window.innerWidth > window.innerHeight) viewBox.firstElementChild.className = 'widescreen';
+				else viewBox.firstElementChild.className = 'portrait';
+
+				viewBox.style.display = "block";
+			}
+			
+		},
+
+		hideViewBox = function(event) {
+			// make sure user clicked viewBox and not any of it's children
+			if (event.target === event.currentTarget || event.target === viewBoxBtn) {
+				viewBox.style.display = "none";
+				document.body.className = "";
+			}
+		};
+	
+
+	// hook up our event listeners
+	window.addEventListener('scroll', getMoreResults);
+	searchField.addEventListener('change', handleSearch);
+	resultsContainer.addEventListener('click', showViewBox);
+	viewBox.addEventListener('click', hideViewBox);
+	viewBoxBtn.addEventListener('click', hideViewBox);
+
+
+	window.onbeforeunload = function() {
+		// cleanup all event listeners before we exit
+		window.removeEventListener('scroll', getMoreResults);
+		searchField.removeEventListener('change', handleSearch);
+		resultsContainer.removeEventListener('click', showViewBox);
+		viewBox.removeEventListener('click', hideViewBox);
+		viewBoxBtn.removeEventListener('click', hideViewBox);
+	}
+
+})();
